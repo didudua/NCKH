7bf889c843cd559af86d9d55b725d6731366f811
didudua <126039750+didudua@users.noreply.github.com>
2025-09-12 10:45:55 +0700

    First commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from bs4 import BeautifulSoup
+import pandas as pd
+import random
+import time
+from selenium.webdriver.chrome.options import Options
+import traceback
